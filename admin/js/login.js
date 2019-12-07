dfaef4363ea28a5c413834a457cf63a3966f19a0
jack <jackjun8090@163.com>
2019-12-07 15:29:25 +0800

    后台登录页面完成

---
+++ admin/js/login.js
@@ -0,0 +1,32 @@
+$(function () {
+    $('.input_sub').on('click', function (e) {
+        e.preventDefault();
+        let userName = $('.input_txt').val().trim();
+        let userPass = $('.input_pass').val().trim();
+        if (userName == '' || userPass == '') {
+            $('.modal-body').text('用户名和密码不能为空！！')
+            $('.modal').modal();
+            return;
+        }
+        $.ajax({
+            url: 'http://localhost:8080/api/v1/admin/user/login',
+            type: 'post',
+            data: {
+                username: userName,
+                password: userPass,
+            },
+            success: function (res) {
+                $('.modal-body').text(res.msg);
+                $('.modal').modal();
+                if (res.code == 200) {
+                    localStorage.setItem('token',res.token);
+                    $('.modal').on('hidden.bs.modal',function(){
+                        location.href = './index.html';
+                    })
+                }
+
+            }
+
+        })
+    })
+})
